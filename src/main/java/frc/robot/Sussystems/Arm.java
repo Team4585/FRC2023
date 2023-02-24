d077644f24659eb4f85a920c32a49431e186bee5
Team-4585 <84157800+Team-4585@users.noreply.github.com>
2023-02-24 14:49:07 -0700

    Updated the arm and WiringConnections to include slavery

--- src/main/java/frc/robot/Sussystems/Arm.java
+++ src/main/java/frc/robot/Sussystems/Arm.java
@@ -9,9 +9,9 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class Arm extends RoboDevice{
   WiringConnections m_TheConnections = new WiringConnections();
 
-private BasicPID left_motor;
-// private BasicPID right_motor;
-// private CANSparkMax intakeMotor;
+private BasicPID master_Motor;
+private BasicPID slave_Motor_1;
+private BasicPID slave_Motor_2;
 
 private double targPosition = 2.0;
 private double armSpeed = 0.0;
@@ -21,7 +21,9 @@ private static double positionThreshold = 0.05;
   public Arm(){
     super("Arm Sub System");
     //Comments are from wallE class from last year
-    left_motor = new BasicPID(WiringConnections.TEST_ARM_MOTOR_ID);
+    master_Motor = new BasicPID(WiringConnections.MASTER_ARM_CONTROLLER_ID);
+    slave_Motor_1 = new BasicPID(WiringConnections.SLAVE_1_ARM_CONTROLLER_ID);
+    slave_Motor_2 = new BasicPID(WiringConnections.SLAVE_2_ARM_CONTROLLER_ID);
     // right_motor = new BasicPID(WiringConnections.RIGHT_WALLE_CONTROLLER_ID);
   
     // left_motor.setSlave(right_motor);
@@ -35,8 +37,14 @@ private static double positionThreshold = 0.05;
 
   public void testArm(){
     //Update the double for specific values
-    left_motor.setRotations(targPosition + left_motor.getPosition());
+    master_Motor.setRotations(targPosition + master_Motor.getPosition());
     System.out.println("Testing the arm");
+  }
+
+
+
+  public void raiseArm(){
+    master_Motor.setRotations(-targPosition + master_Motor.getPosition());
   }
 
   // public boolean isDetracted(){
